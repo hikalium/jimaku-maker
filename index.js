0081b8eeb4746b9ea8770ba5beef7de8f0130754
hikalium <hikalium@hikalium.com>
2019-02-17 00:42:47 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+var file = document.getElementById('file');
+var canvas = document.getElementById('canvas');
+var caption_text_box = document.getElementById('captionTextBox');
+var uploadImgSrc;
+
+var ctx = canvas.getContext('2d');
+
+caption_text_box.addEventListener('change', () => {
+  console.log(caption_text_box.value);
+  generate();
+}, false);
+
+function loadLocalImage(e) {
+  var fileData = e.target.files[0];
+  if (!fileData.type.match('image.*')) {
+    alert('画像を選択してください');
+    return;
+  }
+
+  var reader = new FileReader();
+  reader.onload = function() {
+    uploadImgSrc = reader.result;
+    canvasDraw();
+  } reader.readAsDataURL(fileData);
+}
+
+file.addEventListener('change', loadLocalImage, false);
+var img;
+function canvasDraw(imgSrc) {
+  img = new Image();
+  img.src = uploadImgSrc;
+  img.onload = function() {
+    generate();
+  }
+}
+
+function generate() {
+  var w = img.width;
+  var h = img.height;
+
+  if (w / 16 * 9 < h)
+    h = w / 16 * 9;
+  else
+    w = h / 9 * 16
+    console.log(img.width);
+  console.log(img.height);
+  console.log(w);
+  console.log(h);
+  canvas.width = w;
+  canvas.height = h;
+  ctx.clearRect(0, 0, canvas.width, canvas.width);
+  ctx.drawImage(img, -(img.width - w) / 2, -(img.height - h) / 2);
+
+  var sh = canvas.width / 2.35;
+  var space_height = (canvas.height - sh) / 2;
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, space_height);
+  ctx.fillRect(0, space_height + sh, canvas.width, space_height);
+  addText();
+
+  var data = canvas.toDataURL();
+  var outputImg = document.createElement('img');
+  outputImg.src = data;
+  document.getElementById('result').appendChild(outputImg);
+}
+
+function addText() {
+  var font_size = canvas.height / 20;
+  ctx.font = `bold ${font_size}px 'MS Pゴシック'`;
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = 'white';
+  ctx.fillText(
+      caption_text_box.value, canvas.width / 2,
+      canvas.height - font_size * 1.5);
+  ctx.strokeStyle = 'black';
+  ctx.strokeWidth = 2;
+  ctx.strokeText(
+      caption_text_box.value, canvas.width / 2,
+      canvas.height - font_size * 1.5);
+}
